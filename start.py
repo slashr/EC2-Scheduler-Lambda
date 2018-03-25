7934b29354e448d5b33f62e4a4bff3e1a2f49d13
slashr <slashr@users.noreply.github.com>
2018-03-25 20:58:04 +0200

    Fixed typos

--- start.py
+++ start.py
@@ -10,18 +10,18 @@ ec2 = boto3.resource('ec2')
 
 def lambda_handler(event, context):
 
-	#Filter instance having specified tag and status running
+	#Filter instance having specified tag and status stopped
 	filter = [{'Name': 'tag:AutoStop', 'Values': ['Yes', 'yes', 'YES']}, {'Name': 'instance-state-name','Values': ['stopped']}]
 	instances = ec2.instances.filter(Filters=filter)
 
-	#Get IDs of all running and tagged instances
+	#Get IDs of all stopped and tagged instances
 	instance_ids = [instance.id for instance in instances]
 
-	#Print list of instances that will be stopped to logs
+	#Print list of instances that will be started to logs
 	print instance_ids
 
 	if len(instance_ids) > 0:
-		#Stop instances
+		#Start instances
 		starting_instances = ec2.instances.filter(InstanceIds=instance_ids).start()
 		print starting_instances
 	else:
